03fa3cb4f8a696583c985a0cf480181ff3d08509
blackboxAI <anubhavnanda@Anubhavs-MacBook-Pro.local>
2020-04-03 00:28:23 +0530

    Assignment submission

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,52 +3,78 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.ListIterator;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private static final String TAG = DetailActivity.class.getName();
 
+    private ImageView ingredientsIv;
+    private TextView mainNametv;
+    private TextView alsoKnownAstv;
+    private TextView placeOfOrigintv;
+    private TextView descriptiontv;
+    private TextView ingredientstv;
+
+
+    private Sandwich sandwich;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ingredientsIv = findViewById(R.id.image_iv);
+
+        placeOfOrigintv = (TextView) findViewById(R.id.origin_tv);
+        descriptiontv = (TextView)findViewById(R.id.description_tv);
+        ingredientstv = (TextView)findViewById(R.id.ingredients_tv);
+        alsoKnownAstv = (TextView)findViewById(R.id.also_known_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
+            Log.d(TAG,"Intent is null");
             closeOnError();
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
+            Log.d(TAG,"Extra position not found in intent");
             closeOnError();
             return;
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
+            Log.d(TAG,"Sandwich is null");
             closeOnError();
             return;
         }
 
         populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
 
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(sandwich.getMainName());
+
     }
 
     private void closeOnError() {
@@ -57,6 +83,17 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(ingredientsIv);
+
+        placeOfOrigintv.setText(sandwich.getPlaceOfOrigin());
+        descriptiontv.setText(sandwich.getDescription());
+        ingredientstv.setText(TextUtils.join(", ", sandwich.getIngredients()));
+        alsoKnownAstv.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,72 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JsonUtils {
 
+        private  static final String TAG = JsonUtils.class.getName();
+
+        private static  final String NAME_CODE = "name";
+        private static final String MAIN_NAME_CODE = "mainName";
+        private static final String ALSO_KNOWN_AS_CODE = "alsoKnownAs";
+        private static final String PLACE_OF_ORIGIN_CODE = "placeOfOrigin";
+        private static final String DESCRIPTION_CODE = "description";
+        private static final String IMAGE_CODE = "image";
+        private static final String INGREDIENTS_CODE = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Log.d(TAG,json);
+
+        try {
+            JSONObject obj = new JSONObject(json);
+
+            JSONObject name = obj.getJSONObject(NAME_CODE);
+            String mainName = name.getString(MAIN_NAME_CODE);
+
+            JSONArray JSONArrayAlsoKnownAs = name.getJSONArray(ALSO_KNOWN_AS_CODE);
+            List<String> alsoKnownAs = convertToListFromJSONArray(JSONArrayAlsoKnownAs);
+
+            String placeOfOrigin = obj.getString(PLACE_OF_ORIGIN_CODE);
+            String description = obj.getString(DESCRIPTION_CODE);
+            String image = obj.getString(IMAGE_CODE);
+
+
+            JSONArray JSONIngredients= obj.getJSONArray(INGREDIENTS_CODE);
+            List<String> ingredients = convertToListFromJSONArray(JSONIngredients);
+
+            return new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+    private static List<String> convertToListFromJSONArray(JSONArray jsonArray) throws JSONException{
+
+            List<String> list = new ArrayList<String>();
+
+            for(int i=0;i<jsonArray.length();i++){
+                list.add(jsonArray.getString(i));
+            }
+
+            return list;
+
     }
 }
